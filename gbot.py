9c3d6f26ca76c049ba1a2c75fb13d3bf750244fc
gamah <deadon22087@gmail.com>
2015-10-24 02:11:21 -0500

    case-insensitize smug checks

--- gbot.py
+++ gbot.py
@@ -79,7 +79,7 @@ class commands:
 
     def smug(info):
         s = "Fuck you, "
-        if (("Gamah" in info['msg']) or (NICK in info['msg'])):
+        if (("gamah" in str.lower(info['msg'])) or (str.lower(NICK) in str.lower(info['msg']))):
             s += info['user']
         else:
             s += info['msg'][len(info['botcmd']) + 1:]
